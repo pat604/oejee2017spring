c3ebee5670de65c702ee108ec22d71e7c5de782b
kovta <p.kovacstamas@gmail.com>
2017-03-12 17:13:52 +0100

    Revert "Construction of service based core persistence layer with elementary functionality. Minor schema refactoring. Creation of overview purposed web layer."
    This reverts commit 6f533886346b280edd3b152113e4af346cdb0167.

--- projects/stratagem/strat-persistence/src/main/java/com/kota/stratagem/persistence/query/ProjectQuery.java
+++
@@ -1,7 +0,0 @@
-package com.kota.stratagem.persistence.query;
-
-public class ProjectQuery {
-	
-	public static final String GET_ALL_PROJECTS = "Projects.getAll"; 
-	
-}
--- projects/stratagem/strat-persistence/src/main/java/com/kota/stratagem/persistence/query/TaskQuery.java
+++
@@ -1,7 +0,0 @@
-package com.kota.stratagem.persistence.query;
-
-public class TaskQuery {
-	
-	public static final String GET_ALL_TASKS = "Tasks.getAll"; 
-	
-}
