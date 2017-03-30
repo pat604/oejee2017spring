e745bf530727ca2b81d709c53a0abc21f75bdc41
SebestyenMiklos <sebestyenmiklos1neptun@gmail.com>
2017-03-30 10:42:02 +0200

    Dateconverter and Dateconverter unit tests added

---
+++ projects/stmm/stmm-ejb/src/test/java/hu/smiklos/stmm/ejb/converter/DateConverterTest.java
@@ -0,0 +1,67 @@
+package hu.smiklos.stmm.ejb.converter;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by SebestyenMiklos on 2017. 03. 29..
+ */
+public class DateConverterTest {
+
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void getDateAsContinouesString() throws Exception {
+        String string = "01 2, 2010";
+        DateFormat format = new SimpleDateFormat("mm d, yyyy", Locale.ENGLISH);
+        Date date = format.parse(string);
+        String dateString = DateConverter.getDateAsContinouesString(date);
+        Assert.assertEquals("20100102", dateString);
+    }
+
+    @Test
+    public void getDateHyphenSeparated() throws Exception {
+        String string = "02 02, 2010";
+        DateFormat format = new SimpleDateFormat("mm dd, yyyy", Locale.ENGLISH);
+        Date date = format.parse(string);
+        String dateString = DateConverter.getDateHyphenSeparated(date);
+        Assert.assertEquals("2010-02-02", dateString);
+    }
+
+    @Test
+    public void getDateAsContinouesStringDecember() throws Exception {
+        String string = "12 2, 2010";
+        DateFormat format = new SimpleDateFormat("mm d, yyyy", Locale.ENGLISH);
+        Date date = format.parse(string);
+        String dateString = DateConverter.getDateAsContinouesString(date);
+        Assert.assertEquals("20101202", dateString);
+    }
+
+    @Test
+    public void getDateHyphenSeparatedDecember() throws Exception {
+        String string = "12 2, 2010";
+        DateFormat format = new SimpleDateFormat("mm d, yyyy", Locale.ENGLISH);
+        Date date = format.parse(string);
+        String dateString = DateConverter.getDateHyphenSeparated(date);
+        Assert.assertEquals("2010-12-02", dateString);
+    }
+
+}
