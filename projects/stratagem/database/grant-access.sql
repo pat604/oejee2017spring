522b503aaa7daad67cb4f0b3bdc9ea1a1fc5ff54
Kota <p.kovacstamas@gmail.com>
2017-04-10 23:55:47 +0200

    Core entities created with preliminary fields

--- projects/stratagem/database/grant-access.sql
+++ projects/stratagem/database/grant-access.sql
@@ -1,8 +1,8 @@
 GRANT SELECT, INSERT, UPDATE, DELETE ON app_users, roles, authorizations TO stratagem_std_user;
 GRANT USAGE, SELECT, UPDATE ON app_users_user_id_seq, roles_role_id_seq, authorizations_authorization_id_seq TO stratagem_std_user;
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON projects, tasks, project_tasks TO stratagem_std_user;
-GRANT USAGE, SELECT, UPDATE ON tasks_task_id_seq, projects_project_id_seq TO stratagem_std_user;
+GRANT SELECT, INSERT, UPDATE, DELETE ON objectives, projects, tasks, objective_projects, project_tasks TO stratagem_std_user;
+GRANT USAGE, SELECT, UPDATE ON objectives_objective_id_seq, projects_project_id_seq, tasks_task_id_seq TO stratagem_std_user;
 
 GRANT SELECT, INSERT, UPDATE, DELETE ON user_project_assignments TO stratagem_std_user;
 
---
+++ projects/stratagem/strat-persistence/src/main/java/com/kota/stratagem/persistence/query/ObjectiveQuery.java
@@ -0,0 +1,10 @@
+package com.kota.stratagem.persistence.query;
+
+public class ObjectiveQuery {
+
+	public static final String COUNT_BY_ID = "Objectives.countById";
+	public static final String GET_ALL_OBJECTIVES = "Objectives.getAll";
+	public static final String GET_BY_ID = "Objectives.getById";
+	public static final String REMOVE_BY_ID = "Objectives.removeById";
+	
+}
