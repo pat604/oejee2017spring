f7a764911e45891fc3079371b1ffc327f47b93fe
Tamas Mitro <blacktom73@gmail.com>
2017-04-29 13:42:18 +0200

    Guitar GET REST services added to the project

---
+++ projects/showyourguitar/show-webservice/src/main/java/hu/mitro/webservice/main/GuitarRestApplication.java
@@ -0,0 +1,8 @@
+package hu.mitro.webservice.main;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class GuitarRestApplication extends Application {
+}
