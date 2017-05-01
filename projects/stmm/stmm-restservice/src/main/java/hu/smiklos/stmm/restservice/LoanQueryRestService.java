e7e4550bfd91829a25a7bc4ae6afe7ec6c5cd851
SebestyenMiklos <sebestyenmiklos1neptun@gmail.com>
2017-05-01 11:28:33 +0200

    Loan offer get service added to restapi

--- projects/stmm/stmm-restservice/src/main/java/hu/smiklos/stmm/restservice/LoanQueryRestService.java
+++ projects/stmm/stmm-restservice/src/main/java/hu/smiklos/stmm/restservice/LoanQueryRestService.java
@@ -1,11 +1,15 @@
 package hu.smiklos.stmm.restservice;
 
+import hu.smiklos.stmm.ejb.domain.LoanOfferForRestStub;
 import hu.smiklos.stmm.ejb.domain.OfferListOnBorrowQuery;
 import hu.smiklos.stmm.ejb.facade.BorrowFacadeInterface;
+import hu.smiklos.stmm.pers.entity.MoneyTransfer;
 import hu.smiklos.stmm.pers.exception.PersistenceServiceException;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by SebestyenMiklos on 2017. 05. 01..
@@ -17,9 +21,19 @@ public class LoanQueryRestService implements LoanQueryRestServiceInterface {
     private BorrowFacadeInterface borrowFacade;
 
     @Override
-    public OfferListOnBorrowQuery getOffers(String repayment_type) throws PersistenceServiceException {
+    public List<LoanOfferForRestStub> getOffers(String repayment_type) throws PersistenceServiceException {
 
         OfferListOnBorrowQuery result = borrowFacade.getOffers(repayment_type);
-        return result;
+        List<LoanOfferForRestStub> loanList = new ArrayList<LoanOfferForRestStub>();
+        for(MoneyTransfer transfer : result.getOffers()){
+            loanList.add(new LoanOfferForRestStub(
+                    transfer.getTransfer_amount(),
+                    transfer.getExpected_return_amount(),
+                    transfer.getMoney_transfer_repayment_type().getRepayment_type_name(),
+                    transfer.getExpected_return_amount() - transfer.getTransfer_amount(),
+                    transfer.getMoneytransfer_investment_time_period_month()
+            ));
+        }
+        return loanList;
     }
 }
--- projects/stmm/stmm-restservice/src/main/java/hu/smiklos/stmm/restservice/LoanQueryRestServiceInterface.java
+++ projects/stmm/stmm-restservice/src/main/java/hu/smiklos/stmm/restservice/LoanQueryRestServiceInterface.java
@@ -1,13 +1,14 @@
 package hu.smiklos.stmm.restservice;
 
 import hu.smiklos.stmm.ejb.domain.BorrowStub;
-import hu.smiklos.stmm.ejb.domain.OfferListOnBorrowQuery;
+import hu.smiklos.stmm.ejb.domain.LoanOfferForRestStub;
 import hu.smiklos.stmm.pers.exception.PersistenceServiceException;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import java.util.List;
 
 /**
  * Created by SebestyenMiklos on 2017. 05. 01..
@@ -19,6 +20,6 @@ public interface LoanQueryRestServiceInterface {
     @GET
     @Path("/{"+BorrowStub.BORROW_REPAYMENT_TYPE+"}")
     @Produces("application/json")
-    OfferListOnBorrowQuery getOffers(@PathParam(BorrowStub.BORROW_REPAYMENT_TYPE) String repayment_type) throws PersistenceServiceException;
+    List<LoanOfferForRestStub> getOffers(@PathParam(BorrowStub.BORROW_REPAYMENT_TYPE) String repayment_type) throws PersistenceServiceException;
 
 }
